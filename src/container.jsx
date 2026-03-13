e9b8540094f2ff97d5f04d35460265e8d46712c2
Ajon Bemshima Benedict <ajonbem123@gmail.com>
2026-03-13 22:58:30 +0100

    Cleanup outer project folder to avoid Vercel confusion

--- src/container.jsx
+++
@@ -1,5 +0,0 @@
-export default function Container({ children, className }) {
-  return (
-    <div className={`max-w-[1100px] mx-auto ${className}`}>{children}</div>
-  );
-}
--- src/size.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Size ({className, children, onClick}){
-    return(
-        <div className="flex ">
-            <button onClick={onClick} className={`py-3 px-4 ${className} cursor-pointer border border-black-500`}>{children}</button>
-        </div>
-    );
-}
